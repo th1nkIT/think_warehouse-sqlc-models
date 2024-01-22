db945ae88f32e48f40b2ed53c32af0a1121b37fe
rahadinabudiman <erbees.50000@gmail.com>
2024-01-22 17:38:12 +0700

    Fix: Product Query

--- scripts/pgbo/queries/product.sql
+++ scripts/pgbo/queries/product.sql
@@ -7,10 +7,10 @@ RETURNING product.*;
 
 -- name: UpdateProduct :one
 UPDATE product 
-SET name = @new_name, 
-    product_picture_url = @new_product_picture_url, 
-    description = @new_description, 
-    updated_by = @new_created_by, 
+SET name = @name,
+    product_picture_url = @product_picture_url,
+    description = @description,
+    updated_by = @updated_by,
     updated_at = (now() at time zone 'UTC')::TIMESTAMP 
 WHERE guid = @guid
 RETURNING product.*;
@@ -24,26 +24,34 @@ WHERE
     guid = @guid
   AND deleted_at IS NULL;
 
--- name: ListWithFilter :many
+-- name: ListProduct :many
 SELECT *
 FROM product
 WHERE
     (CASE WHEN @set_name::bool THEN LOWER(name) LIKE LOWER(@name) ELSE TRUE END)
-    AND(CASE WHEN @set_description::bool THEN LOWER(description) LIKE LOWER(@description) ELSE TRUE END)
     AND deleted_at IS NULL
 ORDER BY (CASE WHEN @order_param = 'id ASC' THEN guid END) ASC,
          (CASE WHEN @order_param = 'id DESC' THEN guid END) DESC,
          (CASE WHEN @order_param = 'name ASC' THEN name END) ASC,
          (CASE WHEN @order_param = 'name DESC' THEN name END) DESC,
-         (CASE WHEN @order_param = 'role_id ASC' THEN description END) ASC,
-         (CASE WHEN @order_param = 'role_id DESC' THEN description END) DESC,
          (CASE WHEN @order_param = 'created_at ASC' THEN created_at END) ASC,
          (CASE WHEN @order_param = 'created_at DESC' THEN created_at END) DESC,
          product.created_at DESC
 LIMIT @limit_data
 OFFSET @offset_page;
 
--- name: FindWithGUID :many
-SELECT *
-FROM product
-WHERE guid = @guid;
+-- name: GetProduct :one
+SELECT
+    p.*
+FROM product p
+WHERE
+    p.guid = @guid
+  AND p.deleted_at IS NULL;
+
+
+-- name: GetCountProductList :one
+SELECT COUNT(p.id) FROM product p
+WHERE
+    (CASE WHEN @set_name::bool THEN LOWER(p.name) LIKE LOWER(@name) ELSE TRUE END)
+    AND(CASE WHEN @set_description::bool THEN LOWER(p.description) LIKE LOWER(@description) ELSE TRUE END)
+    AND p.deleted_at IS NULL;
