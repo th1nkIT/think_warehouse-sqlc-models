b5e3c0ad92e2894bdc42c129466887fbf0b05030
rahadinabudiman <erbees.50000@gmail.com>
2024-01-23 18:49:23 +0700

    Update: Fix Warehouse Queries for Filter

--- scripts/pgbo/migrations/000009_create_warehouse_table.up.sql
+++ scripts/pgbo/migrations/000009_create_warehouse_table.up.sql
@@ -9,16 +9,16 @@ CREATE TABLE IF NOT EXISTS warehouse
 (
     id bigint NOT NULL DEFAULT nextval('warehouse_id_seq'::regclass),
     guid character varying NOT NULL,
+    warehouse_code character varying(20) NOT NULL,
     name character varying(100),
     address character varying(100) NOT NULL,
     phone_number character varying(50) NOT NULL,
-    is_active boolean,
     created_at timestamp without time zone NOT NULL,
     created_by character varying NOT NULL,
     updated_at timestamp without time zone,
     updated_by character varying,
     deleted_at timestamp without time zone,
     deleted_by character varying,
-    last_login timestamp without time zone,
-    CONSTRAINT warehouse_pkey PRIMARY KEY (guid)
+    CONSTRAINT warehouse_pkey PRIMARY KEY (guid),
+    CONSTRAINT warehouse_warehouse_code_key UNIQUE (warehouse_code)
 );
