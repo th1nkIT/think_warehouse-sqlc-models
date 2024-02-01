a93f1839b5483b469e40d3f331acacf70746b76e
rahadinabudiman <erbees.50000@gmail.com>
2024-02-01 19:38:46 +0700

    Fix: Get Product

--- scripts/pgbo/queries/product.sql
+++ scripts/pgbo/queries/product.sql
@@ -1,13 +1,14 @@
 -- name: InsertProduct :one
 INSERT INTO product 
-        (guid, category_id, name, is_variant, product_code, product_picture_url, description, created_at, created_by)
+        (guid, category_id, name, product_sku, is_variant, product_code, product_picture_url, description, created_at, created_by)
     VALUES
-        (@guid, @category_id, @name, @is_variant, @product_code, @product_picture_url, @description, (now() at time zone 'UTC')::TIMESTAMP, @created_by)
+        (@guid, @category_id, @name, @product_sku, @is_variant, @product_code, @product_picture_url, @description, (now() at time zone 'UTC')::TIMESTAMP, @created_by)
 RETURNING product.*;
 
 -- name: UpdateProduct :one
 UPDATE product 
 SET name = @name,
+    product_sku = @product_sku,
     is_variant = @is_variant,
     category_id = @category_id,
     product_picture_url = @product_picture_url,
@@ -117,27 +118,42 @@ LIMIT @limit_data
 
 -- name: GetProduct :one
 SELECT
-    p.guid, p.category_id, p.name, p.is_variant, p.product_code, p.product_picture_url, p.description, p.created_at, p.created_by,
-    p.updated_at, p.updated_by, p.deleted_at, p.deleted_by,
-    ub_created.name AS user_name, ub_created.guid AS user_id,
-    ub_updated.name AS user_name_update, ub_updated.guid AS user_id_update,
-    pc.name AS category_name,
-    pp.price, pp.discount_type, pp.discount, pp.is_active,
-    pv.guid AS product_variant_id, pv.name AS product_variant_name, pv.sku AS product_variant_sku
+    p.*,
+    pc.name product_category_name,
+    json_agg(
+            json_build_object(
+                    'product_variant_id', pv.guid,
+                    'name', pv.name,
+                    'sku', pv.sku,
+                    'price', pp.price,
+                    'discount', pp.discount,
+                    'discount_type', pp.discount_type,
+                    'is_active', (CASE WHEN pv.is_active IS NOT NULL THEN pv.is_active WHEN p.deleted_at IS NOT NULL THEN 'inactive' ELSE 'inactive' END),
+                    'stock_id', (SELECT guid FROM stock WHERE product_id = p.guid AND (CASE WHEN pv.guid IS NOT NULL THEN product_variant_id = pv.guid ELSE product_variant_id IS NULL END)),
+                    'stock', (SELECT stock FROM stock WHERE product_id = p.guid AND (CASE WHEN pv.guid IS NOT NULL THEN product_variant_id = pv.guid ELSE product_variant_id IS NULL END)),
+                    'created_at', p.created_at,
+                    'created_by', ub_created.name,
+                    'updated_at', p.updated_at,
+                    'updated_by', ub_updated.name
+            )
+    ) AS product_variant
 FROM
     product p
-        LEFT JOIN user_backoffice ub_created ON ub_created.guid = p.created_by
-        LEFT JOIN user_backoffice ub_updated ON ub_updated.guid = p.updated_by
-        LEFT JOIN product_category pc ON pc.guid = p.category_id
+        LEFT JOIN
+    product_category pc
+    ON
+        p.category_id = pc.guid
         LEFT JOIN
     product_variant pv
-ON
-    pv.product_id = p.guid
-    LEFT JOIN
+    ON
+        pv.product_id = p.guid
+        LEFT JOIN user_backoffice ub_created ON ub_created.guid = p.created_by
+        LEFT JOIN user_backoffice ub_updated ON ub_updated.guid = p.updated_by
+        LEFT JOIN
     product_price pp
     ON
-    pp.product_id = p.guid
-    AND (CASE WHEN pp.product_variant_id IS NULL THEN pp.product_variant_id IS NULL ELSE pp.product_variant_id = pv.guid END)
+        pp.product_id = p.guid
+            AND (CASE WHEN pp.product_variant_id IS NULL THEN pp.product_variant_id IS NULL ELSE pp.product_variant_id = pv.guid END)
 WHERE
     p.guid = @guid;
 
